1ecc55a20e46fab045c34a5f81bfe10a514af033
Sarman-Dasa <sarmand@zignuts.com>
2023-02-22 19:18:24 +0530

    create migration of : country and capital

---
+++ eloquent_relationships/database/migrations/2023_02_22_114331_create_capitals_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('capitals', function (Blueprint $table) {
+            $table->id();
+            $table->string('capital_name');
+            $table->unsignedBigInteger('country_id');
+            $table->timestamps();
+            $table->foreign('country_id')->references('id')->on('countries')->cascadeOnDelete()->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('capitals');
+    }
+};
