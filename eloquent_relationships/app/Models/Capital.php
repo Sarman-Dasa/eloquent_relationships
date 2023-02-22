e0a3df857fb0785b90101838a3dc5c7ff6515f37
Sarman-Dasa <sarmand@zignuts.com>
2023-02-22 19:17:54 +0530

    create modal : capital & country

---
+++ eloquent_relationships/app/Models/Capital.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Capital extends Model
+{
+    use HasFactory;
+
+    protected $fillable =[
+        'capital_name',
+        'country_id'
+    ];
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
+}
